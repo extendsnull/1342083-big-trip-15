7ab1aa44564cdca10458177646b707051424ee15
extendsnull <extendsnull@gmail.com>
2021-07-26 21:46:39 +0300

    Конфигурирует сборщик

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const path = require(`path`);
+
+module.exports = {
+  entry: `./src/main.js`,
+  output: {
+    filename: `bundle.js`,
+    path: path.resolve(__dirname, `public`),
+  },
+  devtool: `source-map`,
+};
