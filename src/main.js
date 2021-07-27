5445da5184c9430b441b31f26f6f157466286b51
extendsnull <extendsnull@gmail.com>
2021-07-27 14:52:51 +0300

    Рендерит компоненты на странице

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import {EVENTS_COUNT} from './const';
+import {render} from './utils';
+import * as view from './view';
+
+const mainContainer = document.querySelector('.trip-main');
+const navigationContainer = document.querySelector('.trip-controls__navigation');
+const filtersContainer = document.querySelector('.trip-controls__filters');
+const contentContainer = document.querySelector('.trip-events');
+
+const eventItems = Array.from(new Array(EVENTS_COUNT), () => view.getEventTemplate());
+
+render(mainContainer, view.getTripInfoTemplate(), 'afterbegin');
+render(navigationContainer, view.getTripNavigationTemplate());
+render(filtersContainer, view.getTripFiltersTemplate());
+
+render(contentContainer, view.getTripSortTemplate());
+render(contentContainer, view.getEventListTemplate(
+  view.getEditEventTemplate(),
+  view.getAddEventTemplate(),
+  ...eventItems,
+));
