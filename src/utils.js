00916a51cf3fcac9c95d0bc21dc03362ad3df8ab
extendsnull <extendsnull@gmail.com>
2021-08-02 17:26:52 +0300

    Убирает лишние дефисы

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,8 @@
 /**
  * Отрисовывает компоненты.
- * @param {HTMLElement} element - элемент, по отношению к которому происходит вставка компонента.
- * @param {string} html - разметка для компонента.
- * @param {InsertPosition} where - местоположение компонента (см. {@link https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML insertAdjacentHTML}). Значение по-умолчанию: `beforeend`.
+ * @param {HTMLElement} element элемент, по отношению к которому происходит вставка компонента.
+ * @param {string} html разметка для компонента.
+ * @param {InsertPosition} where местоположение компонента (см. {@link https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML insertAdjacentHTML}). Значение по-умолчанию: `beforeend`.
  */
 export const render = (element, html, where = 'beforeend') => {
   element.insertAdjacentHTML(where, html);
