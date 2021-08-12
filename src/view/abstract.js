46e39d514403d4e117fa3181dd5b94f5450efe93
extendsnull <extendsnull@gmail.com>
2021-08-12 22:31:03 +0300

    Добавляет абстрактный класс для View

---
+++ src/view/abstract.js
@@ -0,0 +1,27 @@
+import {createElement} from '../utils';
+
+export default class Abstract {
+  constructor() {
+    if (new.target === Abstract) {
+      throw new Error('Cannot create an instance of an abstract class.');
+    }
+
+    this._element = null;
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    throw new Error(`Non-abstract class '${this.constructor.name}' does not implement inherited abstract member 'getTemplate' from class 'Abstract'.`);
+  }
+}
