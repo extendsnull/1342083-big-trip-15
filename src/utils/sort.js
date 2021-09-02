523fd5aa7ac5588d648700020d6ac6c46c0606bf
extendsnull <extendsnull@gmail.com>
2021-09-02 18:13:45 +0300

    Обновляет утилиты сортировки

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,9 +1,11 @@
 import { getDatesDiff } from './date';
 
-export const sortByDuration = (items) => items.slice().sort((item1, item2) => {
+export const sortByDateFrom = (item1, item2) => item1.dateFrom - item2.dateFrom;
+
+export const sortByDuration = (item1, item2) => {
   const diff1 = getDatesDiff(item1.dateFrom, item1.dateTo);
   const diff2 = getDatesDiff(item2.dateFrom, item2.dateTo);
   return diff2 - diff1;
-});
+};
 
-export const sortByPrice = (items) => items.slice().sort((item1, item2) => item2.basePrice - item1.basePrice);
+export const sortByPrice = (item1, item2) => item2.basePrice - item1.basePrice;
