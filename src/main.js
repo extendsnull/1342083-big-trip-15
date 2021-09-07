45854f7cc13747e697f3af2ab10e97f9e67427c0
extendsnull <extendsnull@gmail.com>
2021-09-07 12:09:13 +0300

    Инкапсулирует разные презентеры внутри общего

--- src/main.js
+++ src/main.js
@@ -1,55 +1,10 @@
 import PointsModel from './models/points';
 import FilterModel from './models/filter';
-import NavigationPresenter from './presenters/navigation';
-import InfoPresenter from './presenters/info';
-import AddButtonPresenter from './presenters/add-button';
-import FilterPresenter from './presenters/filter';
-import BoardPresenter from './presenters/board';
-import StatsPresenter from './presenters/stats';
+import MainPresenter from './presenters/main';
 import { mockPoints } from './data';
 
 const pointsModel = new PointsModel();
 const filterModel = new FilterModel();
 pointsModel.addPoints(mockPoints);
 
-const infoPresenter = new InfoPresenter(
-  document.querySelector('.trip-main'),
-  pointsModel,
-);
-
-const filterPresenter = new FilterPresenter(
-  document.querySelector('.trip-controls'),
-  filterModel,
-  pointsModel,
-);
-
-const boardPresenter = new BoardPresenter(
-  document.querySelector('.page-main'),
-  pointsModel,
-  filterModel,
-);
-
-const addButtonPresenter = new AddButtonPresenter(
-  document.querySelector('.trip-main'),
-  boardPresenter,
-);
-
-const statsPresenter = new StatsPresenter(
-  document.querySelector('.page-main'),
-  pointsModel,
-  filterPresenter,
-);
-
-const navigationPresenter = new NavigationPresenter(
-  document.querySelector('.trip-controls'),
-  boardPresenter,
-  statsPresenter,
-  pointsModel,
-  filterModel,
-);
-
-infoPresenter.init();
-filterPresenter.init();
-addButtonPresenter.init();
-boardPresenter.init();
-navigationPresenter.init();
+new MainPresenter(pointsModel, filterModel);
---
+++ src/presenters/main.js
@@ -0,0 +1,76 @@
+import NavigationPresenter from '../presenters/navigation';
+import InfoPresenter from '../presenters/info';
+import AddButtonPresenter from '../presenters/add-button';
+import FilterPresenter from '../presenters/filter';
+import BoardPresenter from '../presenters/board';
+import StatsPresenter from '../presenters/stats';
+
+const containers = {
+  info: document.querySelector('.trip-main'),
+  filter: document.querySelector('.trip-controls'),
+  board: document.querySelector('.page-main'),
+  addButton: document.querySelector('.trip-main'),
+  stats: document.querySelector('.page-main'),
+  navigation: document.querySelector('.trip-controls'),
+};
+
+export default class MainPresenter {
+  constructor(pointsModel, filterModel) {
+    this._pointsModel = pointsModel;
+    this._filterModel = filterModel;
+    this._presenters = {};
+
+    this._init();
+  }
+
+  _init() {
+    this._createPresenters();
+    this._initPresenters();
+  }
+
+  _initPresenters() {
+    this._presenters.info.init();
+    this._presenters.filter.init();
+    this._presenters.addButton.init();
+    this._presenters.board.init();
+    this._presenters.navigation.init();
+  }
+
+  _createPresenters() {
+    this._presenters.info= new InfoPresenter(
+      containers.info,
+      this._pointsModel,
+    );
+
+    this._presenters.filter= new FilterPresenter(
+      containers.filter,
+      this._filterModel,
+      this._pointsModel,
+    );
+
+    this._presenters.board= new BoardPresenter(
+      containers.board,
+      this._pointsModel,
+      this._filterModel,
+    );
+
+    this._presenters.addButton= new AddButtonPresenter(
+      containers.addButton,
+      this._presenters.board,
+    );
+
+    this._presenters.stats= new StatsPresenter(
+      containers.stats,
+      this._pointsModel,
+      this._presenters.filter,
+    );
+
+    this._presenters.navigation= new NavigationPresenter(
+      containers.navigation,
+      this._presenters.board,
+      this._presenters.stats,
+      this._pointsModel,
+      this._filterModel,
+    );
+  }
+}
